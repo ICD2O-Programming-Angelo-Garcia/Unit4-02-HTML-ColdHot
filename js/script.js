5c9c311e845331a38fdf4b3c88115a37205d9be4
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-05-01 14:35:38 +0000

    Sentence about what you did

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This function is triggered when the user clicks "Click to Check"
+function checkTemperature() {
+  // Get the temperature entered by the user
+  const temperature = parseInt(document.getElementById("temperature-input").value);
+
+  // Validate if the input is a valid number
+  if (isNaN(temperature)) {
+    document.getElementById("result").innerHTML = "Please enter a valid number for temperature.";
+    return; // Stop the function if the input is invalid
+  }
+
+  // Check if the temperature is high (hot) or low (cold)
+  if (temperature >= 25) {
+    document.getElementById("result").innerHTML = "It's hot outside, go outside!";
+  } else {
+    document.getElementById("result").innerHTML = "It's cold outside, stay inside!";
+  }
+}
